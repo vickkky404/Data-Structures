cbad27afb8307e03a17a6974f12d0c3aa4e709d3
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-09 19:57:47 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+      for(int i=1 ; i<= 4 ; i++){
+          for(int j = 1 ; j<= i ; j++){
+              System.out.print(j);
+              
+          }
+           System.out.println();
+      
+      }
+    }
+}
