9fc3b96422b5639c84b451cc724045c674d4367b
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-10 11:49:41 +0530

    Add files via upload

---
+++ PairsInCpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int main() {
+    pair<int,int> arr[] = {{1,2} , {3,5} , {9,8}};
+    cout << arr[0].first << " "<<endl;
+    cout << arr[0].second;
+
+    return 0;
+}
