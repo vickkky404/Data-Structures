8e1139ee0242546789d407edafa6d2d96b8707f8
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-14 18:50:26 +0530

    Add files via upload
    Traversal of array in java

---
+++ ArrayTraversal.java
@@ -0,0 +1,12 @@
+public class ArrayTraversal {
+    public static void main(String[] args) {
+        int[] arr = {10 , 20 , 30 , 40 , 50};
+        
+        // traversal 
+        System.out.println("Array Traversal");
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}
